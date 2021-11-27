7719168c0fb9e42fcf7da7bd9f325dcdbe16d869
Sasha Ermohin <saha22023@gmail.com>
2021-11-27 14:58:55 +0700

    bot base

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+import yaml
+import disnake
+from disnake.ext import commands
+
+# Setting up Logging
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)s][%(name)s]: %(message)s',
+    level=logging.INFO
+)
+log = logging.getLogger('bot')
+
+# Loading configurations
+cfg = yaml.safe_load(open('config.yml', 'r'))
+
+log.info('Starting disnake {0} {1}...'.format(
+    disnake.__version__, disnake.version_info.releaselevel
+))
+
+# Initialize Bot Class
+intents = disnake.Intents.none()
+intents.guilds = True
+intents.guild_messages = True
+bot = commands.Bot(
+    command_prefix=cfg['bot']['prefix'],
+    help_command=None,
+    intents=intents
+)
+
+# Running Bot from Bot Token
+bot.run(token=cfg['bot']['token'])
